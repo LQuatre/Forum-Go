8de37652f098c5bad8815d8f93c4e50f75e46189
abder <ismail.benlaidi@ynov.com>
2024-06-17 17:25:11 +0200

    add css

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById('loginForm').style.display = 'block';
+    document.getElementById('createAccountForm').style.display = 'none';
+}
+
+function showCreateAccount() {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('createAccountForm').style.display = 'block';
+}
