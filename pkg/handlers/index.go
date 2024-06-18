e836b945ede425baa831d8453470f9922f7f523c
djeanbap <jeanbaptiste.delbeau@ynov.com>
2024-06-18 17:14:21 +0200

    add categoris layout

--- pkg/handlers/index.go
+++ pkg/handlers/index.go
@@ -7,7 +7,6 @@ import (
 )
 
 func Index(writer http.ResponseWriter, request *http.Request) {
-<<<<<<< HEAD
 	topics, err := models.GetAllTopics()
 	if err != nil {
 		http.Redirect(writer, request, "/err?msg=Cannot get topics", http.StatusTemporaryRedirect)
@@ -21,26 +20,12 @@ func Index(writer http.ResponseWriter, request *http.Request) {
 	}
 }
 
-func Err(writer http.ResponseWriter, request *http.Request)  {
-    vals := request.URL.Query()
-    _, err := session(writer, request)
-    if err != nil {
-        generateHTML(writer, vals.Get("msg"), "layout", "navbar", "error")
-    } else {
-        generateHTML(writer, vals.Get("msg"), "layout", "auth.navbar", "error")
-    }
+func Err(writer http.ResponseWriter, request *http.Request) {
+	vals := request.URL.Query()
+	_, err := session(writer, request)
+	if err != nil {
+		generateHTML(writer, vals.Get("msg"), "layout", "navbar", "error")
+	} else {
+		generateHTML(writer, vals.Get("msg"), "layout", "auth.navbar", "error")
+	}
 }
-=======
-	generateHTML(writer, "", "layout", "navbar", "index")
-}
-
-// func Err(writer http.ResponseWriter, request *http.Request)  {
-//     vals := request.URL.Query()
-//     _, err := session(writer, request)
-//     if err != nil {
-//         generateHTML(writer, vals.Get("msg"), "layout", "navbar", "error")
-//     } else {
-//         generateHTML(writer, vals.Get("msg"), "layout", "auth.navbar", "error")
-//     }
-// }
->>>>>>> 0923d2acd6e71947a5c5ea2dbce5be0bb65ce63e
--- pkg/models/db.go
+++ pkg/models/db.go
@@ -46,4 +46,3 @@ func Encrypt(plaintext string) (cryptext string) {
 	cryptext = fmt.Sprintf("%x", sha1.Sum([]byte(plaintext)))
 	return
 }
-
