f9f29ad8534253385519ccfe33a9061b76398397
abder <ismail.benlaidi@ynov.com>
2024-06-19 17:21:27 +0200

    Create initial database schema

---
+++ sql.sql
@@ -0,0 +1,55 @@
+CREATE TABLE categories (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uuid TEXT NOT NULL,
+    name TEXT NOT NULL,
+    created_at DATETIME NOT NULL
+) AUTO_INCREMENT=1;
+
+CREATE TABLE topics (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uuid TEXT NOT NULL,
+    name TEXT NOT NULL,
+    category_id INTEGER NOT NULL,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY(category_id) REFERENCES categories(id)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE threads (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uuid TEXT NOT NULL,
+    topic_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY(topic_id) REFERENCES topics(id),
+    FOREIGN KEY(user_id) REFERENCES users(id)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uuid TEXT NOT NULL,
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    thread_id INTEGER NOT NULL,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(thread_id) REFERENCES threads(id)
+) AUTO_INCREMENT=1;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME NOT NULL
+) AUTO_INCREMENT=1;
+
+CREATE TABLE sessions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    user_id INT NOT NULL,
+    created_at DATETIME NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+) AUTO_INCREMENT=1;
