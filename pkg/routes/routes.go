b9a6bcf93679b8aa7fdadc71e0e36bd5c31785cf
abder <ismail.benlaidi@ynov.com>
2024-06-19 17:21:00 +0200

    chore: Update navbar styling and category page layout

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -7,79 +7,104 @@ import (
 )
 
 type WebRoute struct {
-    Name        string
-    Method      string
-    Pattern     string
-    HandlerFunc http.HandlerFunc
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
 }
 
 type WebRoutes []WebRoute
 
 var webRoutes = WebRoutes{
 	{
-        "home",
-        "GET",
-        "/",
-        handlers.Index,
-    },
-    {
-        "signup",
-        "GET",
-        "/signup",
-        handlers.Signup,
-    },
-    {
-        "signupAccount",
-        "POST",
-        "/signup_account",
-        handlers.SignupAccount,
-    },
-    {
-        "login",
-        "GET",
-        "/login",
-        handlers.Login,
-    },
-    {
-        "auth",
-        "POST",
-        "/authenticate",
-        handlers.Authenticate,
-    },
-    {
-        "logout",
-        "GET",
-        "/logout",
-        handlers.Logout,
-    },
-    // {
-    //     "newThread",
-    //     "GET",
-    //     "/thread/new",
-    //     handlers.NewThread,
-    // },
-    // {
-    //     "createThread",
-    //     "POST",
-    //     "/thread/create",
-    //     handlers.CreateThread,
-    // },
-    // {
-    //     "readThread",
-    //     "GET",
-    //     "/thread/read",
-    //     handlers.ReadThread,
-    // },
-    // {
-    //     "postThread",
-    //     "POST",
-    //     "/thread/post",
-    //     handlers.PostThread,
-    // },
-    {
-        "error",
-        "GET",
-        "/err",
-        handlers.Err,
-    },
+		"home",
+		"GET",
+		"/",
+		handlers.Index,
+	},
+	{
+		"signup",
+		"GET",
+		"/signup",
+		handlers.Signup,
+	},
+	{
+		"signupAccount",
+		"POST",
+		"/signup_account",
+		handlers.SignupAccount,
+	},
+	{
+		"login",
+		"GET",
+		"/login",
+		handlers.Login,
+	},
+	{
+		"auth",
+		"POST",
+		"/authenticate",
+		handlers.Authenticate,
+	},
+	{
+		"logout",
+		"GET",
+		"/logout",
+		handlers.Logout,
+	},
+	{
+		"newThread",
+		"GET",
+		"/thread/new",
+		handlers.NewThread,
+	},
+	{
+		"createThread",
+		"POST",
+		"/thread/create",
+		handlers.CreateThread,
+	},
+	{
+		"readThread",
+		"GET",
+		"/thread/read",
+		handlers.ReadThread,
+	},
+
+	{
+		"postThread",
+		"POST",
+		"/thread/post",
+		handlers.PostThread,
+	},
+	{
+		"Category",
+		"GET",
+		"/category",
+		handlers.Category,
+	},
+	{
+		"newCategory",
+		"GET",
+		"/category/new",
+		handlers.NewCategory,
+	},
+	{
+		"createCategory",
+		"POST",
+		"/category/create",
+		handlers.CreateCategory,
+	},
+	{
+		"goCategory",
+		"GET",
+		"/category/category",
+		handlers.GoCategory,
+	},
+	{
+		"error",
+		"GET",
+		"/err",
+		handlers.Err,
+	},
 }
